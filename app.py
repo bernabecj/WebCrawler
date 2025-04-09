37077dbe5124486a3c8b5edbd866cbde59d1b96d
bernabecj <62186957+bernabecj@users.noreply.github.com>
2025-04-09 15:54:38 -0500

    refacto: update port number

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ routes.register_routes_views.register_routes(app)
 
 if __name__ == "__main__":
     """
-    Corremos el servidor en el puerto 5100 y debug = True para que el servidor
+    Corremos el servidor en el puerto 80 y debug = True para que el servidor
     se reinicie cuando se hagan cambios
 
-    All hosts: host="0.0.0.0", port=5100
-    Open your browser and type: 127.0.0.1:5100
+    All hosts: host="0.0.0.0", port=80
+    Open your browser and type: 127.0.0.1:80
     """
-    app.run(debug=True, host="0.0.0.0", port=5100)
+    app.run(debug=True, host="0.0.0.0", port=80)
